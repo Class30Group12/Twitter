d7f0038c859a017199dd47d85bf9787ec439d03d
Godswill <gozienso@yahoo.com>
2022-11-25 21:09:57 +0000

    sql file second commit

--- db.sql
+++ db.sql
@@ -1,1 +1,23 @@
 Learning git is very interesting
+
+This is a project for Class30Group12.
+
+1.  ceziekwe
+2.  Chuntie
+3.  Demobosco
+4.  Edial30
+5.  ENOH123
+6.  Gozienso
+7.  Janmesaidoo32
+8.  KingTech30
+9.  LeoTechllc
+10. madee01
+11. opepeace
+12. TangweElvis
+13. Titidevops
+14. guy-roger (has not accpted invitation as the time of making this list).
+15. 
+16. 
+17. 
+18.
+
